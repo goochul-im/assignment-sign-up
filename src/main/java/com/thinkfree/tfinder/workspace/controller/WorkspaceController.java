e416259dd795c3589f4c66261b436083e1c8ec25
goochul.im <goochul.im@thinkfree.com>
2026-04-22 17:14:21 +0900

    feat: WorkspaceController 추가

--- src/main/java/com/thinkfree/tfinder/workspace/controller/WorkspaceController.java
+++ src/main/java/com/thinkfree/tfinder/workspace/controller/WorkspaceController.java
@@ -2,6 +2,10 @@ package com.thinkfree.tfinder.workspace.controller;
 
 import com.thinkfree.tfinder.workspace.service.iface.IWorkspaceUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkspaceController {
 
     private final IWorkspaceUseCase workspaceUseCase;
+
+    @GetMapping("/invite/accept")
+    public ResponseEntity<?> inviteAccept(@RequestParam String token){
+
+        return new ResponseEntity<>();
+    }
+
+    @PostMapping("/invite")
+    public ResponseEntity<?> inviteMember() {
+
+
+
+        return new ResponseEntity<>();
+    }
 
 }
