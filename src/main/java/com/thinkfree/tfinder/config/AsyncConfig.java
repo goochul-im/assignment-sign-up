c736120756554d330ffe3c0474e98c9a4e86d6b7
goochul.im <goochul.im@thinkfree.com>
2026-04-22 10:35:45 +0900

    feat: 비동기 스레드풀 설정

---
+++ src/main/java/com/thinkfree/tfinder/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package com.thinkfree.tfinder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig { // 비동기 스레드를 위해 설정
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 최소한으로 유지하는 스레드 수
+        executor.setMaxPoolSize(10); // 스레드 풀 최대 갯수
+        executor.setQueueCapacity(100); // 작업 큐 용량 수
+        executor.initialize();
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // 작업 큐가 꽉 찼거나 큐를 사용할수 없어서 거부될 때 정책
+                                                                                         // 호출한 스레드에서 거부된 task를 대신 실행함
+        return executor;
+    }
+
+
+}
