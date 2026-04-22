d234be29a1afbe1ae7a31258ba4b3cf7a4882c90
goochul.im <goochul.im@thinkfree.com>
2026-04-22 14:53:35 +0900

    feat: MemberRepository 구현 작성

--- src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/MemberRepository.java
+++ src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/MemberRepository.java
@@ -1,23 +1,38 @@
 package com.thinkfree.tfinder.workspace.infrastructure.persistence.adapter;
 
 import com.thinkfree.tfinder.workspace.domain.Member;
+import com.thinkfree.tfinder.workspace.infrastructure.persistence.entity.MemberEntity;
 import com.thinkfree.tfinder.workspace.infrastructure.persistence.iface.IMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository implements IMemberRepository {
+
+    private final MemberJpaRepository memberJpaRepository;
+
     @Override
     public Member save(Member member) {
-        return null;
+        return memberJpaRepository.save(MemberEntity.fromDomain(member)).toDomain();
     }
 
     @Override
     public Member findById(Long id) {
-        return null;
+        return memberJpaRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("") // TODO: 예외처리 수정
+        ).toDomain();
     }
 
     @Override
     public boolean isExistByEmail(String email) {
-        return false;
+        return memberJpaRepository.existsByEmail(email);
+    }
+
+    @Override
+    public Member findByEmail(String email) {
+        return memberJpaRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("") // TODO: 예외처리 수정
+        ).toDomain();
     }
 }
