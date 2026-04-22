ea5761ed25beffdd40bfb015f2f78fe8c4f41dcd
goochul.im <goochul.im@thinkfree.com>
2026-04-22 13:22:12 +0900

    test: 초대 토큰 기능 테스트

---
+++ src/test/java/com/thinkfree/tfinder/workspace/service/adapter/JwtManagerTest.java
@@ -0,0 +1,68 @@
+package com.thinkfree.tfinder.workspace.service.adapter;
+
+import com.thinkfree.tfinder.workspace.service.dto.InviteTokenResult;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtManagerTest {
+
+    private static JwtManager jwtManager;
+
+    @BeforeAll
+    static void before() {
+        jwtManager = new JwtManager("my-secret-key-Lorem-ipsum-dolor-sit-amet-sollicitudin-vel-dapibus-magna-condimentum. ");
+    }
+
+    @Test
+    void 초대_토큰이_정상적으로_만들어지고_정보를_파싱할수_있어야_한다(){
+        //given
+        String to = "to@email.com";
+        String from = "from@email.com";
+        String url = "1234-1234";
+        Instant expTime = Instant.now().plusSeconds(1000);
+        String token = jwtManager.generateInviteToken(from, to, url, expTime);
+
+        //when
+        InviteTokenResult result = jwtManager.parsingInviteToken(token);
+
+        //then
+        assertThat(result.fromEmail()).isEqualTo(from);
+        assertThat(result.toEmail()).isEqualTo(to);
+        assertThat(result.workspaceUrl()).isEqualTo(url);
+    }
+
+    @Test
+    void 초대_토큰이_만료되었을_경우_예외를_반환해야_한다(){
+        //given
+        String to = "to@email.com";
+        String from = "from@email.com";
+        String url = "1234-1234";
+        Instant expTime = Instant.now().minusSeconds(1000);
+        String token = jwtManager.generateInviteToken(from, to, url, expTime);
+
+        //when & then
+        assertThrows(RuntimeException.class,() -> jwtManager.parsingInviteToken(token));
+
+    }
+
+    @Test
+    void 초대_토큰이_아닐_경우_예외를_반환해야_한다(){
+        //given
+        Instant expTime = Instant.now().plusSeconds(1000);
+        String token = jwtManager.generateAccessToken(1L, expTime);
+
+        //when & then
+        assertThrows(RuntimeException.class, () -> jwtManager.parsingInviteToken(token));
+    }
+
+
+}
