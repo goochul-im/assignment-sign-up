0070c133c16e1e82f082e3d7aaef89e2d9a5ac7c
goochul.im <goochul.im@thinkfree.com>
2026-04-22 17:36:26 +0900

    in-progress: IAuthUseCase 구현

--- src/main/java/com/thinkfree/tfinder/auth/service/adapter/AuthService.java
+++ src/main/java/com/thinkfree/tfinder/auth/service/adapter/AuthService.java
@@ -1,4 +1,17 @@
 package com.thinkfree.tfinder.auth.service.adapter;
 
-public class AuthService {
+import com.thinkfree.tfinder.auth.service.dto.SignupDto;
+import com.thinkfree.tfinder.auth.service.iface.IAuthUseCase;
+import com.thinkfree.tfinder.workspace.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements IAuthUseCase {
+
+    @Override
+    public Member signUp(SignupDto dto) {
+        return null;
+    }
 }
--- src/main/java/com/thinkfree/tfinder/auth/service/iface/IAuthUseCase.java
+++ src/main/java/com/thinkfree/tfinder/auth/service/iface/IAuthUseCase.java
@@ -1,9 +1,10 @@
 package com.thinkfree.tfinder.auth.service.iface;
 
+import com.thinkfree.tfinder.auth.service.dto.SignupDto;
 import com.thinkfree.tfinder.workspace.domain.Member;
 
 public interface IAuthUseCase {
 
-    Member signUp();
+    Member signUp(SignupDto dto);
 
 }
