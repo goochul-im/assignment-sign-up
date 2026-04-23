756307511be2924e4d6900c886c76ab0abefc3f6
goochul.im <goochul.im@thinkfree.com>
2026-04-23 14:14:25 +0900

    test: 회원가입 및 서비스 단위 테스트 작성

---
+++ src/test/java/com/thinkfree/tfinder/auth/service/adapter/AuthServiceTest.java
@@ -0,0 +1,149 @@
+package com.thinkfree.tfinder.auth.service.adapter;
+
+import com.thinkfree.tfinder.auth.service.dto.LoginDto;
+import com.thinkfree.tfinder.auth.service.dto.LoginResult;
+import com.thinkfree.tfinder.auth.service.dto.SignupDto;
+import com.thinkfree.tfinder.auth.service.iface.IAuthUseCase;
+import com.thinkfree.tfinder.common.exception.BusinessException;
+import com.thinkfree.tfinder.common.service.iface.IJwtManager;
+import com.thinkfree.tfinder.workspace.domain.Member;
+import com.thinkfree.tfinder.workspace.domain.MemberType;
+import com.thinkfree.tfinder.workspace.infrastructure.persistence.iface.IMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    private PasswordEncoder encoder;
+    @Mock
+    private IMemberRepository memberRepository;
+    @Mock
+    private IJwtManager jwtManager;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    void 회원가입이_정상적으로_완료되어야_한다(){
+        //given
+        String email = "test@email.com";
+        String name = "test";
+        String passwd = "testPasswd";
+        String encodePasswd = "encoded";
+        SignupDto dto = new SignupDto(
+                email,
+                name,
+                passwd
+        );
+
+        Member returnMember = new Member(
+                1L,
+                name,
+                email,
+                encodePasswd,
+                MemberType.DEFAULT
+        );
+
+        when(memberRepository.isExistByEmail(any())).thenReturn(false);
+        when(encoder.encode(passwd)).thenReturn(encodePasswd);
+        when(memberRepository.save(any())).thenReturn(returnMember);
+
+        //when
+        Member member = authService.signUp(dto);
+
+        //then
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getEmail()).isEqualTo(email);
+        assertThat(member.getName()).isEqualTo(name);
+        assertThat(member.getPassword()).isEqualTo(encodePasswd);
+        assertThat(member.getUserType()).isEqualTo(MemberType.DEFAULT);
+    }
+
+    @Test
+    void 이미_가입된_이메일일_경우_예외를_발생시켜야_한다(){
+        //given
+        String email = "test@email.com";
+        String name = "test";
+        String passwd = "testPasswd";
+        String encodePasswd = "encoded";
+        SignupDto dto = new SignupDto(
+                email,
+                name,
+                passwd
+        );
+
+        when(memberRepository.isExistByEmail(any())).thenReturn(true);
+
+        //when
+        assertThrows(BusinessException.class, () -> authService.signUp(dto));
+    }
+
+    @Test
+    void 로그인이_완료되면_액세스_토큰을_발급해야_한다(){
+        //given
+        String email = "test@email.com";
+        String passwd = "testPasswd";
+        String accessToken = "fake accessToken";
+        LoginDto dto = new LoginDto(
+                email,
+                passwd
+        );
+        Member returnMember = new Member(
+                1L,
+                "name",
+                email,
+                "encodePasswd",
+                MemberType.DEFAULT
+        );
+
+        when(memberRepository.findByEmail(any())).thenReturn(returnMember);
+        when(encoder.matches(any(), any())).thenReturn(true);
+        when(jwtManager.generateAccessToken(any(), any())).thenReturn(accessToken);
+
+        //when
+        LoginResult result = authService.login(dto);
+
+        //then
+        assertThat(result.accessToken()).isEqualTo(accessToken);
+    }
+
+    @Test
+    void 비밀번호가_틀리면_예외를_발생시켜야_한다(){
+        //given
+        String email = "test@email.com";
+        String passwd = "testPasswd";
+        LoginDto dto = new LoginDto(
+                email,
+                passwd
+        );
+        Member returnMember = new Member(
+                1L,
+                "name",
+                email,
+                "encodePasswd",
+                MemberType.DEFAULT
+        );
+
+        when(memberRepository.findByEmail(any())).thenReturn(returnMember);
+        when(encoder.matches(any(), any())).thenReturn(false);
+
+        //when & then
+        assertThrows(BusinessException.class, () -> authService.login(dto));
+    }
+
+
+}
