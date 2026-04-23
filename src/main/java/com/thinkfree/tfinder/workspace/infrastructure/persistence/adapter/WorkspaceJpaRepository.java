4387d00419bf2125f14366b0638e9af5c97d7695
goochul.im <goochul.im@thinkfree.com>
2026-04-23 17:48:07 +0900

    feat: 워크스페이스 리포지토리 기능 연결

--- src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/WorkspaceJpaRepository.java
+++ src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/WorkspaceJpaRepository.java
@@ -4,5 +4,10 @@ import com.thinkfree.tfinder.workspace.domain.Workspace;
 import com.thinkfree.tfinder.workspace.infrastructure.persistence.entity.WorkspaceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WorkspaceJpaRepository extends JpaRepository<WorkspaceEntity, Long> {
+
+    Optional<WorkspaceEntity> findByUrl(String url);
+
 }
--- src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/WorkspaceRepository.java
+++ src/main/java/com/thinkfree/tfinder/workspace/infrastructure/persistence/adapter/WorkspaceRepository.java
@@ -1,24 +1,35 @@
 package com.thinkfree.tfinder.workspace.infrastructure.persistence.adapter;
 
+import com.thinkfree.tfinder.common.exception.BusinessException;
+import com.thinkfree.tfinder.common.exception.ErrorCode;
 import com.thinkfree.tfinder.workspace.domain.Workspace;
+import com.thinkfree.tfinder.workspace.infrastructure.persistence.entity.WorkspaceEntity;
 import com.thinkfree.tfinder.workspace.infrastructure.persistence.iface.IWorkspaceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class WorkspaceRepository implements IWorkspaceRepository {
+
+    private final WorkspaceJpaRepository workspaceJpaRepository;
 
     @Override
     public Workspace save(Workspace workspace) {
-        return null;
+        return workspaceJpaRepository.save(WorkspaceEntity.fromDomain(workspace)).toDomain();
     }
 
     @Override
     public Workspace findById(Long id) {
-        return null;
+        return workspaceJpaRepository.findById(id).orElseThrow(
+                () -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND)
+        ).toDomain();
     }
 
     @Override
     public Workspace findByUrl(String url) {
-        return null;
+        return workspaceJpaRepository.findByUrl(url).orElseThrow(
+                () -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND)
+        ).toDomain();
     }
 }
