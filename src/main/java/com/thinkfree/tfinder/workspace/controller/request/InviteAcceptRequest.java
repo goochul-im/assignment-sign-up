39d9a78df04879a1f6a251ab318dc2b661a19aad
goochul-im <goochul175465@gmail.com>
2026-04-25 22:51:39 +0900

    refactor: validation 추가

--- src/main/java/com/thinkfree/tfinder/workspace/controller/request/InviteAcceptRequest.java
+++ src/main/java/com/thinkfree/tfinder/workspace/controller/request/InviteAcceptRequest.java
@@ -1,6 +1,10 @@
 package com.thinkfree.tfinder.workspace.controller.request;
 
+import com.thinkfree.tfinder.common.constant.ValidationMessage;
+import jakarta.validation.constraints.NotBlank;
+
 public record InviteAcceptRequest(
+        @NotBlank(message = ValidationMessage.NOT_BLANK)
         String inviteToken
 ) {
 }
