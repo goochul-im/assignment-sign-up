a14f560b3af66bdbb1749cac8ad00b630359ce60
goochul-im <goochul175465@gmail.com>
2026-04-25 22:50:47 +0900

    refactor: 네이밍 변경

--- src/main/java/com/thinkfree/tfinder/auth/controller/request/LoginRequest.java
+++ src/main/java/com/thinkfree/tfinder/auth/controller/request/LoginRequest.java
@@ -3,13 +3,12 @@ package com.thinkfree.tfinder.auth.controller.request;
 import com.thinkfree.tfinder.common.constant.ValidationMessage;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
-        @NotBlank(message = ValidationMessage.NOT_BLANK_MESSAGE)
+        @NotBlank(message = ValidationMessage.NOT_BLANK)
         @Email(message = "이메일 형식이 올바르지 않습니다")
         String email,
-        @NotBlank(message = ValidationMessage.NOT_BLANK_MESSAGE)
+        @NotBlank(message = ValidationMessage.NOT_BLANK)
         String password
 ) {
 }
