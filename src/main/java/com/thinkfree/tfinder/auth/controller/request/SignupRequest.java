13c1bc43ac016927bbbe0503cbd461d78cb20d89
goochul-im <goochul175465@gmail.com>
2026-04-25 13:22:06 +0900

    chore: 네이밍 변경

--- src/main/java/com/thinkfree/tfinder/auth/controller/request/SignupRequest.java
+++ src/main/java/com/thinkfree/tfinder/auth/controller/request/SignupRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.*;
 public record SignupRequest(
         @NotBlank(message = ValidationMessage.NOT_BLANK_MESSAGE)
         @Size(min = 2, max = 10, message = "이름은 2글자 이상 10글자 이하여야 합니다")
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = ValidationMessage.SPECIAL_CHAR_MESSAGE)
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = ValidationMessage.USERNAME_PATTERN_MESSAGE)
         String username,
         @NotBlank(message = ValidationMessage.NOT_BLANK_MESSAGE)
         @Email(message = ValidationMessage.EMAIL_MESSAGE)
--- src/main/java/com/thinkfree/tfinder/common/constant/ValidationMessage.java
+++ src/main/java/com/thinkfree/tfinder/common/constant/ValidationMessage.java
@@ -8,7 +8,7 @@ public final class ValidationMessage {
 
     public static final String NOT_BLANK_MESSAGE = "비어있거나 공백은 허용되지 않습니다.";
     public static final String EMAIL_MESSAGE = "이메일 형식이 올바르지 않습니다.";
-    public static final String SPECIAL_CHAR_MESSAGE = "특수문자는 사용할 수 없습니다";
+    public static final String USERNAME_PATTERN_MESSAGE = "한글, 영어, 숫자의 조합만 허용됩니다";
 
 
 }
