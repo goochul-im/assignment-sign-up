e941a0e7ae1a1d666d7c522249a046cf9cd107f6
goochul-im <goochul175465@gmail.com>
2026-04-26 18:20:45 +0900

    docs: CreateWorkspaceResponse에 스웨거 설정 추가

--- src/main/java/com/thinkfree/tfinder/workspace/controller/response/CreateWorkspaceResponse.java
+++ src/main/java/com/thinkfree/tfinder/workspace/controller/response/CreateWorkspaceResponse.java
@@ -1,6 +1,9 @@
 package com.thinkfree.tfinder.workspace.controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateWorkspaceResponse(
+        @Schema(description = "생성된 워크스페이스 ID", example = "100")
         long workspaceId
 ) {
 }
