9ef5563f93772214993d9fae8a6fe17f4577dced
goochul.im <goochul.im@thinkfree.com>
2026-05-11 13:30:00 +0900

    test: 메일 리트라이 기능 테스트

---
+++ src/test/java/com/thinkfree/tfinder/workspace/infrastructure/external/adpater/MailSenderRetryTest.java
@@ -0,0 +1,30 @@
+package com.thinkfree.tfinder.workspace.infrastructure.external.adpater;
+
+import com.thinkfree.tfinder.annotation.IntegrationTest;
+import com.thinkfree.tfinder.workspace.infrastructure.external.iface.IMailSender;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailSendException;
+import org.springframework.resilience.annotation.Retryable;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@IntegrationTest
+class MailSenderRetryTest {
+
+    @Autowired
+    private IMailSender mailSender;
+
+    @Test
+    void Retryable_어노테이션으로_재시도를_할_수_있다(){
+        //given & when
+        assertThrows(MailSendException.class, () -> mailSender.send("to", "title", "message"));
+
+    }
+
+
+}
